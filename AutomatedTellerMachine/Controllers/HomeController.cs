1507b11aabfad90c05351683ea677d8d8cd59126
Schnyder21 <36770138+Schnyder21@users.noreply.github.com>
2018-05-26 23:21:01 -0400

    Add project files.

---
+++ AutomatedTellerMachine/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomatedTellerMachine.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.TheMessage = "Contact us for help navigating this site.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string message)
+        {
+            ViewBag.Message = "Your message has been received.";
+
+            return View();
+        }
+
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASPNETBONE";
+            if (letterCase=="lower")
+            {
+                return Content(serial.ToLower());
+            }
+            return Content(serial);
+        }
+    }
+}
---
+++ AutomatedTellerMachine/Models/CheckingAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AutomatedTellerMachine.Models
+{
+    public class CheckingAccount
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        [Column(TypeName ="varchar")]
+        [RegularExpression(@"\d{6,10}", ErrorMessage="Account number must be 6 to 10 digits.")]
+        [Display(Name ="Account #")]
+        public string AccountNumber{ get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Balance { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+
+        [Required]
+        public string ApplicationUserId { get; set; }
+    }
+}
